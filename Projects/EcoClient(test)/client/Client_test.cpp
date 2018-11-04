47ef1154429e8c16fd72d62e77f6991dae047505
gomtange12 <blueballoon1@naver.com>
2018-11-04 21:11:27 +0900

    에코클라추가

---
+++ Projects/EcoClient(test)/client/Client_test.cpp
@@ -0,0 +1,86 @@
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SERVERIP   "127.0.0.1"
+#define SERVERPORT 9000
+#define BUFSIZE    1024
+// BUFSIZE가 작으면 지금 소켓이 블로킹소켓이기 때문에
+// 작으면 그만큼 블락되는 시간이 적은거고 크면 더길어짐
+//대기시간을 줄이고 용량도 크면 좋음!
+
+#pragma pack(1)
+class Test
+{
+public:
+	bool redy = false;
+};
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (char *)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+int main(int argc, char *argv[])
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// socket()
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) err_quit("socket()");
+
+	// connect()
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = inet_addr(SERVERIP);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = connect(sock, (SOCKADDR *)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("connect()");
+
+	Test files;
+
+	while (true)
+	{
+		retval = send(sock, (char*)&files, sizeof(files), 0);//구조체 보냄
+		if (retval == SOCKET_ERROR) {
+			err_display("send()");
+		}
+
+	}
+	// closesocket()
+	closesocket(sock);
+
+	// 윈속 종료
+	WSACleanup();
+
+	return 0;
+}
--- Projects/server/server/Server.cpp
+++ Projects/server/server/Server.cpp
@@ -6,6 +6,12 @@
 #define SERVERPORT 9000
 #define BUFSIZE    1024
 
+#pragma pack(1)
+class Test {
+public:
+	bool Redy = false;
+};
+
 // 소켓 함수 오류 출력 후 종료
 void err_quit(char *msg)
 {
@@ -53,6 +59,33 @@ int recvn(SOCKET s, char *buf, int len, int flags)
 	return (len - left);
 }
 
+DWORD WINAPI ProcessClient(LPVOID arg) {
+	SOCKET ClientSock = (SOCKET)arg;
+	SOCKADDR ClientAddr;
+	int AddrLen;
+	AddrLen = sizeof(ClientAddr);
+	getpeername(ClientSock, (SOCKADDR*)&ClientAddr, &AddrLen);
+
+	Test files;
+	int retval;
+
+	while (true)
+	{
+		retval = recvn(ClientSock, (char*)&files, sizeof(files), 0);
+		if (retval == SOCKET_ERROR) {
+			err_display("recv()");
+			break;
+		}
+
+		printf("%d", files.Redy);
+
+	}
+	
+	closesocket(ClientSock);
+	return 0;
+}
+
+
 int main(int argc, char *argv[])
 {
 	int retval;
@@ -63,42 +96,44 @@ int main(int argc, char *argv[])
 		return 1;
 	
 	// socket()
-	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
+	SOCKET ListenSock = socket(AF_INET, SOCK_STREAM, 0);
+	if (ListenSock == INVALID_SOCKET) err_quit("socket()");
 
 	// bind()
-	SOCKADDR_IN serveraddr;
-	ZeroMemory(&serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddr.sin_port = htons(SERVERPORT);
-	retval = bind(listen_sock, (SOCKADDR *)&serveraddr, sizeof(serveraddr));
+	SOCKADDR_IN ServerAddr;
+	ZeroMemory(&ServerAddr, sizeof(ServerAddr));
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	ServerAddr.sin_port = htons(SERVERPORT);
+	retval = bind(ListenSock, (SOCKADDR *)&ServerAddr, sizeof(ServerAddr));
 	if (retval == SOCKET_ERROR) err_quit("bind()");
 
 	// listen()
-	retval = listen(listen_sock, SOMAXCONN);
+	retval = listen(ListenSock, SOMAXCONN);
 	if (retval == SOCKET_ERROR) err_quit("listen()");
 
 	// 데이터 통신에 사용할 변수
-	SOCKET client_sock;
-	SOCKADDR_IN clientaddr;
+	SOCKET ClientSock;
+	SOCKADDR_IN ClientAddr;
 	int addrlen;
 
 	while (1)
 	{
-		addrlen = sizeof(clientaddr);
-		client_sock = accept(listen_sock, (SOCKADDR *)&clientaddr, &addrlen);
-		if (client_sock == INVALID_SOCKET) {
+		addrlen = sizeof(ClientAddr);
+		ClientSock = accept(ListenSock, (SOCKADDR *)&ClientAddr, &addrlen);
+		if (ClientSock == INVALID_SOCKET) {
 			err_display("accept()");
 			exit(1);
 		}
 		// 접속한 클라이언트 정보 출력
 		printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
-			inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+			inet_ntoa(ClientAddr.sin_addr), ntohs(ClientAddr.sin_port));
+
+		CreateThread(NULL, 0, ProcessClient, (LPVOID)ClientSock, 0, NULL);
 	}
 
 	// closesocket()
-	closesocket(listen_sock);
+	closesocket(ListenSock);
 
 	// 윈속 종료
 	WSACleanup();
