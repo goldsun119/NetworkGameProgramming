f5583d5ed13d07a2fa01a0a4ddef1017b4852954
gomtange12 <blueballoon1@naver.com>
2018-11-08 09:47:44 +0900

    D

---
+++ Projects/server/server/stdafx.h
@@ -0,0 +1,38 @@
+// stdafx.h : 자주 사용하지만 자주 변경되지는 않는
+// 표준 시스템 포함 파일 또는 프로젝트 관련 포함 파일이
+// 들어 있는 포함 파일입니다.
+//
+
+#pragma once
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "Enemy.h"
+#include "Player.h"
+
+
+#define SERVERPORT 9000
+#define BUFSIZE    1024
+
+using namespace std;
+// TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
+
+enum Scene {
+	E_MENU,
+	E_INGAME,
+	E_GAMEOVER,
+	E_RANK
+};
+
+enum Key
+{
+	E_LEFT,
+	E_RIGHT,
+	E_UP,
+	E_DOWN,
+	E_ATTACK,
+	E_ULTIMATE
+};
