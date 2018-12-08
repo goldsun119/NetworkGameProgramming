315c8e8ac90617ec31670718f4b4178b56dd05ca
LeeSoHyun\LeeSo <leesohyun129@naver.com>
2018-12-09 01:09:24 +0900

    Update Enemy.h

--- Projects/server/server/Enemy.h
+++ Projects/server/server/Enemy.h
@@ -14,9 +14,12 @@ public:
 	bool alive;
 	bool Boss1_Appear = false;
 	bool Boss2_Appear = false;
+	bool Boss1_Stop = false;
+	bool Boss2_Stop = false;
 	//vector<CBullet> m_MonsterBullet;
 	int radian[8] = { 0, 45, 90, 135, 180, 225, 270, 315 }; // 0 ¸Ç¿ìÃø
-
+	int radian3[36] = { 5, 15, 25, 35, 45, 55, 65, 75, 85, 95, 105, 115, 125, 135, 145, 155, 165, 175, 185,
+					195, 205, 215, 225, 235, 245, 255, 265, 275, 285, 295, 305, 315, 325, 335, 345, 355 };
 	vector<CBullet> enemy_bullet1;
 	vector<CBullet> enemy_bullet2[3];
 	vector<CBullet> enemy_bullet3[8];
